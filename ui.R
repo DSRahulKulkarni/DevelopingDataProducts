9b3461739f45b480fa65e0f8da5d8a8583bce94f
DSRahulKulkarni <dsrahulkulkarni@gmail.com>
2015-09-27 15:09:18 -0700

    new changes

--- ui.R
+++ ui.R
@@ -6,6 +6,11 @@ shinyUI(fluidPage(
                   numericInput("SecondNumber", "Second Number",0, min = 0, max = 10, step = 1)
     ),
     mainPanel(
+      h2('Documentation: First, Select values for the numeric input widgets on the left in the sidebar panel. Then a reactive output below will display the sum of those two numbers.'),
+      br(),
+      br(),
+      br(),
+      br(),
       h4('The Sum of the 2 numbers you selected is'),
       verbatimTextOutput('sum')
     )
